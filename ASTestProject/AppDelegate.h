dfb13cb18a27d7463074b09c988e86e71e11f888
Andy LaVoy <andy@logcabinapps.com>
2015-01-11 11:11:00 -0600

    Initial Commit

---
+++ ASTestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ASTestProject
+//
+//  Created by Andy LaVoy on 1/11/15.
+//  Copyright (c) 2015 Andy LaVoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ASTestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ASTestProject
+//
+//  Created by Andy LaVoy on 1/11/15.
+//  Copyright (c) 2015 Andy LaVoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
